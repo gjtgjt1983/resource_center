1c7439033f323ff719ede2f4fdf8a141506e7bee
gaojt <gao_jun_tao@126.com>
2015-07-09 13:56:27 +0800

    finish views

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,23 @@
 'use strict';
 
 /* App Module */
+var ResouceCenter = angular.module('ResouceCenter', [
+    'ngRoute',
+    'resouceCenterControllers'
+]);
+
+ResouceCenter.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/projects', {
+                templateUrl: 'partials/projects.html',
+                controller: 'ProjectsListCtrl'
+            }).
+            when('/projects/:project_name', {
+                templateUrl: 'partials/project.html',
+                controller: 'ProjectDetailCtrl'
+            }).
+            otherwise({
+                redirectTo: '/projects'
+            });
+    }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,66 @@
 'use strict';
 
-/* Controllers */
+var resouceCenterControllers = angular.module('resouceCenterControllers', []);
+
+resouceCenterControllers.controller('ProjectsListCtrl', ['$scope', '$http', function ($scope, $http) {
+    $http.get('data/projects.json').success(function (data) {
+        $scope.projects = data.projects;
+    });
+
+}]);
+
+resouceCenterControllers.controller('ProjectDetailCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+    $scope.project_name = $routeParams.project_name;
+    $scope.prefix = $routeParams.prefix;
+    $http.get('data/' + $scope.project_name + '.json').success(function (data) {
+        $scope.columns = [];
+        $scope.file_type = 'dir';
+        var _data = data;
+        if ($scope.prefix) {
+            var _prefix = $scope.prefix.split('/');
+            for (var i = 0; i < _prefix.length; i++) {
+                for (var k in _data) {
+                    if (k == _prefix[i]) {
+                        _data = _data[k];
+                    }
+                }
+            }
+        }
+
+        if (_data instanceof Object) {
+            for (var column in _data) {
+                $scope.columns.push(column);
+            }
+        } else if (_data.constructor == String) {
+            if (_data.match(/(jpg|png|jpeg|gif)$/i)) {
+                $scope.file_type = 'img';
+            } else {
+                $scope.file_type = 'file';
+            }
+        } else {
+            for (var column in _data) {
+                $scope.columns.push(column);
+            }
+        }
+
+        var _url = '/data/projects/' + $scope.project_name + "/" + $scope.prefix;
+        if ($scope.file_type == 'file') {
+            $("#file").load(_url, function (data) {
+                $(this).html(data);
+            })
+        }
+
+        if($scope.file_type == 'img'){
+            $("#img").attr('src', _url);
+        }
+
+        $('#' + $scope.file_type).show();
+
+
+        $scope.is_html = function(column){
+            return $('$preview_' + column).attr('href', 'http://baidu.com').show();
+        }
+
+    });
+
+}]);
